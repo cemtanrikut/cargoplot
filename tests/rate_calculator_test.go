cc39d7dd8f730cf861d30257b59d705f26212376
Cem Tanrikut <cemtanrikut@gmail.com>
2024-12-18 19:45:36 +0100

    add rate calculator test

---
+++ tests/rate_calculator_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"cmd/main.go/internal/models"
+	"cmd/main.go/internal/services"
+	"testing"
+)
+
+func TestRateCalculator(t *testing.T) {
+	rc := services.NewRateCalculator()
+
+	rc.AddPrice(models.PriceEntry{Company: 1, Price: 1000, Origin: "CNSGH", Date: "2023-01-01"})
+	rc.AddPrice(models.PriceEntry{Company: 2, Price: 800, Origin: "CNSGH", Date: "2023-01-02"})
+	rc.AddPrice(models.PriceEntry{Company: 1, Price: 900, Origin: "CNSGH", Date: "2023-01-03"})
+
+	rates := rc.GetRates()
+	if rates["CNSGH"] != 800 {
+		t.Errorf("Expected rate for CNSGH to be 800, got %d", rates["CNSGH"])
+	}
+}
