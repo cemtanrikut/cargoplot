63b0452af148e6f51455103e1f597eddf748a6cd
Cem Tanrikut <cemtanrikut@gmail.com>
2024-12-18 19:43:23 +0100

    listening server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"cmd/main.go/internal/handlers"
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	router := http.NewServeMux()
+	handler := handlers.NewHandler()
+
+	router.HandleFunc("/", handler.HandleRequests)
+
+	fmt.Println("Server is running on :3142")
+	http.ListenAndServe(":3142", router)
 }
